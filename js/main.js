9df5014c2fea922b17178ef701fb3c93b8fb845b
KA <greenkg@gmail.com>
2017-03-12 15:47:04 -0400

    Updated deletions and validation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//initialize google map and add a marker showing location
 function initMap() {
 	var mapCenter = {lat: 40.805, lng: -73.965};
 	var map = new google.maps.Map(document.getElementById('map'), {
@@ -8,34 +10,60 @@ function initMap() {
 		position: mapCenter,
 		map: map
 	});
+	var placeId = getPlaceId(mapCenter);
 }
 
-initMap();
+//use Google Places API to get restaurant's hours
 
+function getPlaceId(mapCenter) {
+	var placesKey = "AIzaSyDalFLB-OppwlIfavQl4BP1ofutoheImEo";
+	var placesURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + mapCenter.lat + "," + mapCenter.lng + "&radius=100&type=restaurant&key=" + placesKey;
+	$.get(placesURL, function(searchResult) {
+		console.log(searchResult.placeId);
+	});
+}
+
+//configure and initialize Firebase
  var config = {
     apiKey: "AIzaSyDy1g81QkpfZXgftjirh7HCom6ndRg_MD8",
     authDomain: "reservation-site-ce4c5.firebaseapp.com",
     databaseURL: "https://reservation-site-ce4c5.firebaseio.com",
     storageBucket: "reservation-site-ce4c5.appspot.com",
     messagingSenderId: "597068067688"
-  };
+};
  
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
+//when a user submits form, check if data is valid, if yes, add to reservation list
 $('form').on('submit', function(e) {
-	e.preventDefault;
-	$('form').validate();
+	e.preventDefault();
 	var userName = $('#userName').val();
 	var userDate = $('#resDate').val();
-	$('#userName').val('');
-	$('#resDate').val('');
-	var reservationsRef = database.ref('reservations');
-	reservationsRef.push({
-		name: userName,
-		date: userDate
-	});
+	if (validate(userName, userDate)) {
+		$('#userName').val('').removeClass('error');
+		$('#resDate').val('').removeClass('error');
+		$('#error').remove();
+		var reservationsRef = database.ref('reservations');
+		reservationsRef.push({
+			name: userName,
+			date: userDate
+		});
+	} else {
+		$('#userName').addClass('error');
+		$('#resDate').addClass('error');
+		$('#make-reservation').append('<p id="error">Please enter a valid name and date</p>');
+	}
+});
+
+//when a user clicks on the trash can icon, delete the reservation on that line
+$('#reservations').on('click', '.fa-trash-o', function(e) {
+	e.preventDefault();
+	var id = $(e.target).closest('tr').attr('id');
+	var reservationId = database.ref('reservations/' + id);
+	reservationId.remove();
+
 });
 
 database.ref('reservations').on("value", function(results) {
@@ -67,3 +95,24 @@ database.ref('reservations').on("value", function(results) {
   		console.log("The read failed: " + errorObject.code);
 	}
 );
+
+
+//function that verifies that valid data is entered. Returns true or false.
+function validate(userName, date) {
+	console.log('invoked');
+	console.log('user: ' + userName.length + " Date: " + date.length);
+	if (userName.length > 0 && date.length > 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//set minimum date as today's date
+function minDate() {
+	var today = new Date().toISOString().split('T')[0];
+	document.getElementById('resDate').setAttribute('min', today);
+	document.getElementById('resDate').setAttribute('value', today);
+}
+
+minDate();
